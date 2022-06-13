52139f4586b55e09fb8eeae33c6c307a049e76a4
sesheta <goern+sesheta@redhat.com>
2022-06-13 23:55:35 +0000

    Routine Docs Update

--- assets/adviser/documentation_options.js
+++ assets/adviser/documentation_options.js
@@ -1,9 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.53.0',
+    VERSION: '0.56.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
